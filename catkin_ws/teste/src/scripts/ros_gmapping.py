8e78c3d429cbc10252db576bcc88e39dfb44651f
Rafael Katri <rkatri20033@gmail.com>
2022-03-31 11:28:01 -0300

    Slam update

--- catkin_ws/teste/src/scripts/ros_gmapping.py
+++ catkin_ws/teste/src/scripts/ros_gmapping.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python
 
+import cv2
 import rospy
+import matplotlib.pyplot as plt 
+import numpy as np
 from nav_msgs.msg import OccupancyGrid
 
+### Dependencias para simulação com turtlebot
+"""
+ /scan já é publicado ao iniciar a simulação, não precisa ser iniciado
+
+ rosrun gmapping slam_gmapping scan:=scan
+ roslaunch explore_lite explore.launch
+"""
+
 def mapa(info):
-    mapa = info[3]
-    map_pub.publish(mapa)
+    global map
+    map = info.data
+    map_pub.publish(map)
 
 if __name__== "__main__":
-    #rospy.init_node("le_scan.py")
-    #rospy.init_node("slam_gmapping")
-    #rospy.init_node("explore_lite")
-    #rospy.init_node("move_base")
+    map = np.zeros((3,3))
+    map_listner = rospy.Subscriber("/map",OccupancyGrid,queue_size=3,callback=mapa)
+    map_pub = rospy.Publisher("/costmap",OccupancyGrid,queue_size=3)
 
     while not rospy.is_shutdown():
-        map_listner = rospy.Subscriber("/map",OccupancyGrid,queue_size=3,callback=mapa)
-        map_pub = rospy.Publisher("/costmap",OccupancyGrid,queue_size=3)
+        plt.imshow(map,cmap="gray")
+        plt.show()
         
 
     
