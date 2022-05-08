4dc7183ef7d44631daf46d3ea07a869346e0a024
Rafael Katri <rkatri20033@gmail.com>
2022-05-08 19:15:06 -0300

    mapping and exploration prototype

---
+++ catkin_ws/exemplos_221/scripts/mapa.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import rospy
+from nav_msgs.msg import OccupancyGrid
+
+def escutou(info):
+    pub.publish(info)
+
+sub = rospy.Subscriber("/map",OccupancyGrid, escutou)
+pub = rospy.Publisher('/costmap',OccupancyGrid)
+
+while not rospy.is_shutdown():
+    print('a')
