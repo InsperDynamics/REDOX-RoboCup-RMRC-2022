259d5cc81abbd4912b4168c1cc7278bcbe47fff7
MekhyW <mefkhytheroboticist@gmail.com>
2022-06-26 00:18:08 -0300

    update bash install

--- BASH INSTALL EVERYTHING/install.sh
+++ BASH INSTALL EVERYTHING/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Before using the script make sure cmake is up-to-date
+
 cd ~
+
 sudo apt update
 sudo apt upgrade
 sudo apt install curl
@@ -14,10 +17,8 @@ source ~/.bashrc
 sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 sudo rosdep init
 rosdep update
-
 cd ~
-sudo apt-get install libzbar-dev
-sudo apt install sox
+
 git clone https://github.com/libsdl-org/SDL
 mkdir build
 cd build
@@ -25,3 +26,17 @@ cd build
 make
 sudo make install
 cd ~
+
+sudo apt-get install libzbar-dev
+
+sudo apt install sox
+
+sudo snap install arduino
+
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+
+sudo reboot
