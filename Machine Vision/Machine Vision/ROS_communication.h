6aa233669cab8149f6f52b4c6bfc3e98630e59f7
MekhyW <mefkhytheroboticist@gmail.com>
2022-06-28 03:27:53 -0300

    serial baud rate now 115200

--- Machine Vision/Machine Vision/ROS_communication.h
+++ Machine Vision/Machine Vision/ROS_communication.h
@@ -8,6 +8,8 @@
 #include <std_msgs/UInt16.h>
 #include <std_msgs/Float32MultiArray.h>
 using namespace std;
+using namespace std::this_thread;
+using namespace std::chrono;
 bool autonomous_mode = false;
 float current_temperature[64] = {0};
 int current_gas = 0;
@@ -31,8 +33,10 @@ void gasCallback(const std_msgs::UInt16& gas) {
 void ConnectROS(int argc, char** argv)
 {
 	system("gnome-terminal -- roscore");
+	sleep_for(seconds(10));
 	system("gnome-terminal -- sudo chmod a+rw /dev/ttyACM0");
-	system("gnome-terminal -- rosrun rosserial_python serial_node.py /dev/ttyACM0");
+	sleep_for(seconds(10));
+	system("gnome-terminal -- rosrun rosserial_python serial_node.py _port:=/dev/ttyACM0 _baud:=115200");
 	ros::init(argc, argv, "redox_main");
 	ros::NodeHandle nodehandle;
 	pub_command = nodehandle.advertise<std_msgs::String>("arduino_command", 1000);
