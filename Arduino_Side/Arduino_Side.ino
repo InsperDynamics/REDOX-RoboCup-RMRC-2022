d1a7d0e50862ad10deaffa55c1ee014b75c9a328
MekhyW <mefkhytheroboticist@gmail.com>
2022-06-28 02:49:57 -0300

    fixed type conversion warnings on Arduino_Side

--- Arduino_Side/Arduino_Side.ino
+++ Arduino_Side/Arduino_Side.ino
@@ -10,21 +10,21 @@ int current_value_1 = 0;
 int current_value_2 = 0;
 std_msgs::UInt16 gas;
 std_msgs::Float32MultiArray temperature;
-void commandCallback(std_msgs::String& command){
+void commandCallback(const std_msgs::String& command){
   current_command = command.data;
 }
-void value1Callback(std_msgs::UInt16& value1){
+void value1Callback(const std_msgs::UInt16& value1){
   current_value_1 = value1.data;
 }
-void value2Callback(std_msgs::UInt16& value2){
+void value2Callback(const std_msgs::UInt16& value2){
   current_value_2 = value2.data;
 }
 ros::NodeHandle nodehandle;
 ros::Publisher pub_temperature("temperature", &temperature);
 ros::Publisher pub_gas("gas", &gas);
-ros::Subscriber<std_msgs::String> sub_command("arduino_command", &commandCallback);
-ros::Subscriber<std_msgs::UInt16> sub_value_1("arduino_value_1", &value1Callback);
-ros::Subscriber<std_msgs::UInt16> sub_value_2("arduino_value_2", &value2Callback);
+ros::Subscriber<std_msgs::String> sub_command("arduino_command", commandCallback);
+ros::Subscriber<std_msgs::UInt16> sub_value_1("arduino_value_1", value1Callback);
+ros::Subscriber<std_msgs::UInt16> sub_value_2("arduino_value_2", value2Callback);
 
 void ControlMotors(String command, int command_parameter_1, int command_parameter_2) {
   if (command == "MoveBasearm"){
