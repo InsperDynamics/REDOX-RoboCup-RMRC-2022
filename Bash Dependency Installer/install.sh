fb99a9dcd0394a145a49ad7b97e4a7339b62d45f
MekhyW <mefkhytheroboticist@gmail.com>
2022-06-28 01:38:25 -0300

    bash installer now installs librealsense and ydlidar-sdk

--- Bash Dependency Installer/install.sh
+++ Bash Dependency Installer/install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Before using the script make sure cmake is up-to-date
+# Before using the script make sure cmake and pip3 are up-to-date
+# Before using the script make sure internet connection is ok
 
 cd ~
 
 sudo apt update
 sudo apt upgrade
+
 sudo apt install curl
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
@@ -27,6 +29,24 @@ make
 sudo make install
 cd ~
 
+git clone https://github.com/JetsonHacksNano/installLibrealsense
+cd installLibrealsense
+./installLibrealsense.sh
+cd ~
+
+sudo apt install cmake pkg-config
+sudo apt-get install python swig
+git clone https://github.com/YDLIDAR/YDLidar-SDK.git
+cd YDLidar-SDK
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+cd ..
+pip3 install .
+cd ~
+
 sudo apt-get install libzbar-dev
 
 sudo apt-get install libcanberra-gtk-module
@@ -42,3 +62,5 @@ sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/v
 sudo apt install code
 
 sudo reboot
+
+# After running this script, you can setup ros workspace, upload the Arduino code, recompile the machine vision interface, and setup VNC.
