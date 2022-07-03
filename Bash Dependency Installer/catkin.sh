3b0503f5693f0ae0002a0eedff177dfce8108b43
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-07-03 02:05:20 -0300

    Create catkin.sh

---
+++ Bash Dependency Installer/catkin.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~
+mkdir --parents catkin_ws/src
+cd catkin_ws
+catkin init
+sudo apt-get install ros-melodic-catkin
+echo "source ~/opt/ros/melodic/setup.bash" >> ~/.bashrc
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
