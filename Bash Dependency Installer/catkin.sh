3e8a5291eaadd3c4b3dab463b8c214ff0fe395ac
MekhyW <mefkhytheroboticist@gmail.com>
2022-07-13 01:45:49 -0300

    Update URDF + catkin.sh

--- Bash Dependency Installer/catkin.sh
+++ Bash Dependency Installer/catkin.sh
@@ -8,3 +8,10 @@ sudo apt-get install ros-melodic-catkin
 echo "source ~/opt/ros/melodic/setup.bash" >> ~/.bashrc
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
+
+cd ~
+sudo apt-get install ros-melodic-realsense2-camera
+cd ~/catkin_ws/src
+git clone https://github.com/YDLIDAR/ydlidar_ros_driver.git ydlidar_ws/src/ydlidar_ros_driver
+cd ..
+catkin_make
